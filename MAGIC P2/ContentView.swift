20bd4b86ddbe52b2f8b163b66826bae3dd653821
sruiz475 <ruizsophia956@gmail.com>
2025-07-08 18:18:52 -0700

    Initial Commit

---
+++ MAGIC P2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MAGIC P2
+//
+//  Created by sruiz25 on 7/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MAGIC P2/MAGIC_P2App.swift
@@ -0,0 +1,17 @@
+//
+//  MAGIC_P2App.swift
+//  MAGIC P2
+//
+//  Created by sruiz25 on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MAGIC_P2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
